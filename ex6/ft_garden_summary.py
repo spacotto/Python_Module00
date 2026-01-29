e5f85ec28881950a2d830609a89a65a846f857c9
spacotto <silvia.pacotto@gmail.com>
2026-01-29 14:31:21 +0100

    Update ft_garden_summary.py

--- ex6/ft_garden_summary.py
+++ ex6/ft_garden_summary.py
@@ -1,11 +1,18 @@
 def ft_garden_summary():
+    # Color variables for readability
+    green = "\033[1;92m"
+    cyan = "\033[1;96m"
+    white = "\033[1;97m"
+    reset = "\033[0m"
+    
     # Variable that stores input str "name"
-    name = input("Enter garden name: ")
+    name = input(f"{white} Enter garden name: {reset}")
     
     # Variable that stores input str "plants" (no need to convert to int)
-    plants = input("Enter number of plants: ")
+    plants = input(f"{white} Enter number of plants: {reset}")
     
     # Print summary: variables + fixed status message
-    print("Garden:", name)
-    print("Plants:", plants)
-    print("Status: Growing well!")
+    print(f"{white} Garden:{reset}", name)
+    print(f"{white} Plants:{reset}", plants)
+    print(f"{green} Status: Growing well!{reset}")
+
