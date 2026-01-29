6864991e35e14b1f8420c70d4d8bf2c6808c1959
Silvia Pacotto <spacotto@c1r6p16.42lehavre.fr>
2026-01-29 16:48:20 +0100

    update ex5

--- ex5/ft_count_harvest_recursive.py
+++ ex5/ft_count_harvest_recursive.py
@@ -1,20 +1,17 @@
-def ft_count_harvest_recursive(current=1, limit=None):
+# Helper function
+def harvest_helper(current, limit):
+    if current > limit:                 # Exit condition
+        return
+    print(" Day", current)
+    harvest_helper(current + 1, limit)
+
+
+def ft_count_harvest_recursive():
     # Color variables for readability
     green = "\033[1;92m"
     white = "\033[1;97m"
     reset = "\033[0m"
 
-    # Initialisation: Only happens on the very first call
-    if limit is None:
-        limit = int(input(f"{white} Days until harvest: {reset}"))
-
-    # Exit condition
-    if current > limit:
-        print(f"{green} Harvest time!{reset}")
-        return
-
-    # Print the current day
-    print(" Day", current)
-
-    # Recursion: 'current' becomes 'current + 1', 'limit' stays the same
-    ft_count_harvest_recursive(current + 1, limit)
+    limit = int(input(f"{white} Days until harvest: {reset}"))
+    harvest_helper(1, limit)
+    print(f"{green} Harvest time!{reset}")
