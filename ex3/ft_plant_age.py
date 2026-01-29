3ed26df57388a9b4ce2d7cdfd0515a42b33c59da
spacotto <silvia.pacotto@gmail.com>
2026-01-29 11:44:46 +0100

    Update ft_plant_age.py

--- ex3/ft_plant_age.py
+++ ex3/ft_plant_age.py
@@ -1,7 +1,15 @@
 def ft_plant_age():
-    age = int(input("Enter plant age in days: "))
+    # Color variables for readability
+    green = "\033[1;92m"
+    cyan = "\033[1;96m"
+    white = "\033[1;97m"
+    reset = "\033[0m"
     
+    # Ask for age using input() and convert str to int
+    age = int(input(f"{white} Enter plant age in days: {reset}"))
+    
+    # Check if the age is greater than 60 or not
     if age > 60:
-        print("Plant is ready to harvest!")
+        print(f"{green} Plant is ready to harvest!{reset}")
     else:
-        print("Plant needs more time to grow.")
+        print(f"{cyan} Plant needs more time to grow.{reset}")
