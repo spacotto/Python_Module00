8f3aeec88184ec8979052e48a177d3247871971b
spacotto <silvia.pacotto@gmail.com>
2026-01-29 14:28:37 +0100

    Update ft_water_reminder.py

--- ex4/ft_water_reminder.py
+++ ex4/ft_water_reminder.py
@@ -6,11 +6,12 @@ def ft_water_reminder():
     reset = "\033[0m"
     
     # Ask for days using input() and convert str to int
-    days = int(input("Days since last watering: "))
+    days = int(input(f"{white} Days since last watering: {reset}"))
     
     # Check if the number of days is greater than 2
     if days > 2:
         print(f"{cyan} Water the plants!{reset}")
     else:
         print(f"{green} Plants are fine{reset}")
+
 
