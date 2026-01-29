f755105dfb27b771f9f2eb3864b4c96a5c3a9326
spacotto <silvia.pacotto@gmail.com>
2026-01-29 14:26:18 +0100

    Update ft_count_harvest_iterative.py

--- ex5/ft_count_harvest_iterative.py
+++ ex5/ft_count_harvest_iterative.py
@@ -1,7 +1,15 @@
 def ft_count_harvest_iterative():
-    limit = int(input("Days until harvest: "))
-    
+    # Color variables for readability
+    green = "\033[1;92m"
+    white = "\033[1;97m"
+    reset = "\033[0m"
+
+    # Convert exit condition from str to int
+    limit = int(input(f"{white} Days until harvest: {reset}"))
+
+    # for loop
     for day in range(1, limit + 1):
-        print("Day", day)
+        print(" Day", day)
         
-    print("Harvest time!")
+    print(f"{green} Harvest time!{reset}")
+
